d2754c591b0001151d15e2c5689691fb78494364
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-06-26 19:57:45 +0300

    upd

--- checkProjector.py
+++ checkProjector.py
@@ -6,13 +6,9 @@ pageNum = 1
 doc = open('test.txt', 'w')
 
 def skipBlank(text):
-    regex = re.compile(r'''\S.+''')
-    matchObject = regex.search(text)
-    if matchObject != None:
-        newText = matchObject.group()
-    else:
-        newText = 'None'
-    return newText
+    while '  ' in text:
+        text = text.replace('  ', ' ')
+    return text.strip()
 
 print(f'reading page # {pageNum}')
 res = requests.get(libUrl)
@@ -21,20 +17,15 @@ soup = bs4.BeautifulSoup(res.text, 'html.parser')
 lectureUrl = soup.select('.card__link')
 for i in range(len(lectureUrl)):
     lectureRes = requests.get(mainUrl + lectureUrl[i].get('href'))
+    print('handling with ' + mainUrl + lectureUrl[i].get('href'))
     lectureRes.raise_for_status()
     lectureSoup = bs4.BeautifulSoup(lectureRes.text, 'html.parser')
 
     lectureTitle = lectureSoup.select('.video-content__title')
-    lectureSpeaker = lectureSoup.select('p > a.link')
-    lectureSpeakerCompany = lectureSoup.select('p > a:nth-child(2)')
-    # lectureSpeakerCompanyLink get href instead of text
-    lectureSpeakerWhoIs = lectureSoup.select('#content > section.video-content > p')
+    lectureSpeaker = lectureSoup.select('#content > section.video-content > p')
 
     doc.write('\n Title: ' + skipBlank(lectureTitle[0].getText()) +
-              '\n Speaker: ' + skipBlank(lectureSpeaker[0].getText()) +
-              '\n Speaker Company: ' + skipBlank(lectureSpeakerCompany[0].getText()) +
-              '\n Speaker Company Link: ' + skipBlank(lectureSpeakerCompany[0].get('href')) +
-              '\n Who is Speaker: ' + skipBlank(lectureSpeakerWhoIs[0].getText()) + '\n'
+              '\n Speaker: ' + skipBlank(lectureSpeaker[0].getText() + '\n')
               )
 
 doc.close()
